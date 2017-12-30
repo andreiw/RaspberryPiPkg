afa19f300573020b83eb815e48b2dcd193a41882
Andrey Warkentin <andrey.warkentin@gmail.com>
2017-12-30 12:49:00 -0500

    RPi3: fix licensing for DwUsbHostDxe
    The Linaro DwUsbHostDxe bits are claimed to be BSD licensed.
    E.g. see http://crypto.nknu.edu.tw/AOSP/android-8.0.0_r4/device/linaro/bootloader/OpenPlatformPkg/Drivers/Usb/DwUsbHostDxe
    
    This seems false. The original code is practically identical to the U-Boot
    driver (https://github.com/siemens/u-boot/blob/master/drivers/usb/host/dwc2.c)
    and looks nothing like the only DWC2 driver I know of that is
    BSD licensed (https://github.com/freebsd/freebsd/blob/master/sys/dev/usb/controller/dwc_otg.c).
    
    Exempli gratia:
    a) wait_for_bit (U-boot) and Wait4Bit (DwUsbHostDxe)
    b) wait_for_chhltd (U-boot) and Wait4Chhltd (DwUsbHostDxe)
    c) chunk_msg (U-boot) and DwHcTransfer (DwUsbHostDxe)
    d) dwc_otg_hc_init (U-Boot) and DwOtgHcInit (DwUsbHostDxe)
    
    Not nice.
    
    Signed-off-by: Andrey Warkentin <andrey.warkentin@gmail.com>

--- Drivers/DwUsbHostDxe/DwcHw.h
+++ Drivers/DwUsbHostDxe/DwcHw.h
@@ -2,14 +2,10 @@
 
     Copyright (c), 2017, Andrey Warkentin <andrey.warkentin@gmail.com>
     Copyright (c) 2015-2016, Linaro Limited. All rights reserved.
+    Copyright (C) 2012 Oleksandr Tymoshenko <gonzo@freebsd.org>
+    Copyright (C) 2014 Marek Vasut <marex@denx.de>
 
-    This program and the accompanying materials
-    are licensed and made available under the terms and conditions of the BSD License
-    which accompanies this distribution.  The full text of the license may be found at
-    http://opensource.org/licenses/bsd-license.php
-
-    THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-    WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+    SPDX-License-Identifier:     GPL-2.0+
 
 **/
 
