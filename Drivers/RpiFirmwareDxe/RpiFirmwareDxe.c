3aee8965dec9ebc14e702e3d275bc4ad8967747d
Andrei Warkentin <andrey.warkentin@gmail.com>
2018-09-30 23:58:53 -0400

    RPi3: Update file headers for RpiFirmwareDxe.
    Reflect ownership/changes.
    
    Signed-off-by: Andrei Warkentin <andrey.warkentin@gmail.com>

--- Drivers/RpiFirmwareDxe/RpiFirmwareDxe.c
+++ Drivers/RpiFirmwareDxe/RpiFirmwareDxe.c
@@ -1,15 +1,18 @@
 /** @file
-  Copyright (c) 2016, Linaro, Ltd. All rights reserved.<BR>
+ *
+ *  Copyright (c) 2017 - 2018, Andrei Warkentin <andrey.warkentin@gmail.com>
+ *  Copyright (c) 2016, Linaro, Ltd. All rights reserved.
+ *
+ *  This program and the accompanying materials
+ *  are licensed and made available under the terms and conditions of the BSD License
+ *  which accompanies this distribution.  The full text of the license may be found at
+ *  http://opensource.org/licenses/bsd-license.php
+ *
+ *  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+ *
+ **/
 
-  This program and the accompanying materials
-  are licensed and made available under the terms and conditions of the BSD License
-  which accompanies this distribution.  The full text of the license may be found at
-  http://opensource.org/licenses/bsd-license.php
-
-  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
-
-**/
 #include <PiDxe.h>
 
 #include <Library/ArmLib.h>
